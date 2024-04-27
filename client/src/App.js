8839e68a43620160ec23c6305fbe06bea69a1725
Sourabh4tech <168256484+Sourabh4tech@users.noreply.github.com>
2024-04-27 11:10:29 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+import Login from "./components/login/login"
+//import Dashboard from './components/dashboard/dashboard';
+
+function App() {
+  
+  return (
+    <div  className="App">
+      <Login/>
+      {/* <Dashboard/> */}
+      </div>
+  );
+}
+
+export default App;
