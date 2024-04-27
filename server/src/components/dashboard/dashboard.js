ef3a3da1c219b0c375e50b086d4d2c088455b273
Sourabh4tech <168256484+Sourabh4tech@users.noreply.github.com>
2024-04-27 11:05:14 +0530

    Add files via upload

---
+++ server/src/components/dashboard/dashboard.js
@@ -0,0 +1,17 @@
+function Dashboard(){
+
+
+    return (
+        <div className="dashboard">
+            <h1>Customizable/Update in a Databse</h1>
+         
+       
+            
+           
+        </div>
+    )
+    }
+    
+    export default Dashboard;
+
+
